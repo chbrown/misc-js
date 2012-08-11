a266d14a48fb8674f0fba83ca9f1a1ba81be75fb
Christopher Brown <io@henrian.com>
2012-08-11 10:39:55 +0000

    Move layout to redraw method in flags.

--- jquery.flags.js
+++ jquery.flags.js
@@ -21,37 +21,42 @@
 
   function Flag($target, anchor, align, parent) {
     var self = this;
-    this.$target = $target;
     this.anchor = anchor;
-    this.align = align;
+    this.$target = $target;
     this.parent = parent;
 
-    this.target_offset = $target.offset(); // { left: 999, top: 999 }
-    this.target_size = $target.measureBox(); // { width: 999, height: 999 }
-    this.$flag = $('<div class="flag" style="' + this.flag_style + '"></div>').appendTo(parent).on('click', function() {
+    this.$flag = $('<div class="flag"></div>').appendTo(parent).on('click', function() {
       self.remove();
     });
     this.bg_color = this.$flag.css('background-color');
-    this.$span = $('<span></span>').appendTo(this.$flag);
-    this.$triangle = $('<div class="triangle" style="' + this.triangle_style + '"></div>').appendTo(this.$flag);
+    this.$sub = $('<div class="subflag"></div>').appendTo(this.$flag);
+    this.$triangle = $('<div class="triangle"></div>').appendTo(this.$flag);
     this.triangle_radius = parseInt(this.$triangle.css('border-top-width'), 10);
   }
-  Flag.prototype.flag_style = 'display: inline-block; position: absolute; cursor: default; ' +
-      'background-color: black; border-radius: 3px; padding: 2px 6px; ' +
-      'color: white; font-size: 90%;';
-  // like a diamond -- half-diameter measure
-  Flag.prototype.triangle_style = 'display: inline-block; position: absolute; border: 6px solid transparent; width: 0; height: 0;';
-
   Flag.prototype.redraw = function() {
+    var self = this,
+      target_offset = this.$target.offset(), // { left: 999, top: 999 }
+      target_size = this.$target.measureBox(); // { width: 999, height: 999 }
     this.$flag.fadeIn(80);
-    this.$span.html(this.html);
+    if (this.onclick) {
+      $('<a href="#"></a>').appendTo(this.$sub.empty()).text(this.text).click(function(ev) {
+        ev.preventDefault();
+        self.onclick();
+      });
+    }
+    else if (this.href) {
+      $('<a href="' + this.href + '"></a>').appendTo(this.$sub.empty()).text(this.text);
+    }
+    else {
+      $('<span></span>').appendTo(this.$sub.empty()).text(this.text);
+    }
     var flag_size = this.$flag.measureBox(); // { width: 999, height: 999 }
 
     if (this.anchor === 'r' || this.anchor === 'l') {
       // handle anchoring (left/right)
       if (this.anchor === 'r') {
         this.$flag.css({
-          left: this.target_offset.left + this.target_size.width,
+          left: target_offset.left + target_size.width,
           'margin-left': this.triangle_radius});
         this.$triangle.css({
           left: -this.triangle_radius,
@@ -60,7 +65,7 @@
       }
       else {
         this.$flag.css({
-          left: (this.target_offset.left - flag_size.width) - this.triangle_radius,
+          left: (target_offset.left - flag_size.width) - this.triangle_radius,
           'margin-right': this.triangle_radius
         });
         this.$triangle.css({
@@ -71,17 +76,14 @@
       }
 
       // handle alignment (top/middle/bottom)
-      if (this.align === 't') {
-        this.$flag.css('top', this.target_offset.top);
+      if (this.anchor === 't') {
+        this.$flag.css('top', target_offset.top);
       }
       else if (this.align === 'm') {
-        var flag_top = this.target_offset.top +
-          (this.target_size.height / 2.0) -
-          (flag_size.height / 2.0);
-        this.$flag.css('top', flag_top);
+        this.$flag.css('top', target_offset.top + (target_size.height / 2) - (flag_size.height / 2));
       }
       else {
-        this.$flag.css('top', this.target_offset.top + this.target_size.height - flag_size.height);
+        this.$flag.css('top', target_offset.top + target_size.height - flag_size.height);
       }
       this.$triangle.css('top', (flag_size.height - (this.triangle_radius * 2)) / 2);
     }
@@ -100,7 +102,9 @@
       flag = new Flag($target, args.anchor || 'r', args.align || 'm', args.parent || document.body);
     }
     $target.data('flag', flag);
-    flag.html = args.html || args.text || args.message || '!!!';
+    flag.href = args.href;
+    flag.onclick = args.onclick;
+    flag.text = args.text || '!!!';
     flag.redraw();
 
     if (args.fade !== undefined) {
@@ -117,8 +121,7 @@
     // @args.attach: $(document.body)'
     // @args.element: $('#text1') | @args.selector: '#text1'
     // @args.text: 'blah'
-    if (typeof args === 'string')
-      args = {text: args};
+    if (typeof args === 'string') args = {text: args};
     return this.each(function() {
       args.element = $(this);
       $.flag(args);
